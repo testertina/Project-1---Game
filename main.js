513f15478c288467af641771424646e61b177762
tgohil04 <tgohil@spartaglobal.co>
2017-05-23 12:22:05 +0100

    HTML: Removed title for gamepage, added button to return to homepage.  CSS: Styled the gamepage with a star gif background, added homepage button in the top center with grey background and black border.  JS: Used the click method for the return to homepage button so when clicked the gamepage is hidden and the homepage is visible.

--- main.js
+++ main.js
@@ -1,28 +1,39 @@
-// window.addEventListener('DOMContentLoaded', function() {
-// 	var button = document.getElementsByClassName("button");
-// 	console.log(button);
-
-
-// });
+// Load JS to webpage.
 
 $(function() {
 	console.log("DOM ready");
 
+	// Hide gamepage, instructions and leaderboard.
 	$("#gamepage").css("visibility", "hidden");
 	$("#instructions").css("visibility", "hidden");
 	$("#scoreboard").css("visibility", "hidden");
 
-
+	// Click event to begin game. Upon click of "begin game" button the gamepage becomes visible.
 	$("#beginGame").click(function (event) {
   	
   		$("#homepage").hide("slow", function () {
   			$("#gamepage").css("visibility", "visible");
+  			// Make a quit button which returns user back to homepage when clicked.
+  			$("#quit").click(function (event) {
+  				$("#gamepage").css("visibility", "hidden");
+  				$("#homepage").show("slow", function () {
+  				});	
+  			});
+
+  			// Spaceship needs to appear
   			
+
+  			// Asteroids need to fly across screen
+  			
+  			// XP bar needs to increase
+
+  			// Points bar needs to increase
 
   		});
 
   	});
 
+	// Click event to show instructions. Upon click of "how to play" button the instruction page becomes visible.
 	$("#howToPlay").click(function (event) {
   	
   		$("#homepage").hide("slow", function () {
@@ -32,6 +43,7 @@ $(function() {
   		});
   	});
 
+	// Click event to show leaderboard. Upon click of "leaderboard" button the leaderboard page becomes visible.
   	$("#leaderboard").click(function (event) {
   	
   		$("#homepage").hide("slow", function () {
